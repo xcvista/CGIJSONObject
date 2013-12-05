bbeafb06ff0978712b6a8d3e54abdc63c10b7877
Maxthon Chan <xcvista@me.com>
2013-12-05 13:21:54 +0800

    CGIJSONObject running solo

---
+++ CGIJSONObject Tests/CGITestRemoteObject.h
@@ -0,0 +1,19 @@
+//
+//  CGITestRemoteObject.h
+//  CGIJSONObject
+//
+//  Created by Maxthon Chan on 8/15/13.
+//  Copyright (c) 2013 muski. All rights reserved.
+//
+
+#import <CGIJSONObject/CGIJSONObject.h>
+
+@interface CGITestRemoteObject : CGIRemoteObject
+
+@end
+
+@interface CGITestRemoteObject (CGIRemoteMethods)
+
+- (NSArray *)remoteMethod;
+
+@end
---
+++ CGIJSONObject/CGIJSONObject.h
@@ -0,0 +1,22 @@
+//
+//  CGIJSONObject.h
+//  CGIJSONObject
+//
+//  Created by Maxthon Chan on 8/1/13.
+//  Copyright (c) 2013 muski. All rights reserved.
+//
+
+#if __OBJC__
+
+#import <MSBooster/MSBooster.h>
+
+#import <CGIJSONObject/NSObject+CGIJSONSerializable.h>
+#import <CGIJSONObject/NSObject+CGIJSONSerializing.h>
+#import <CGIJSONObject/NSArray+CGIJSONSerializable.h>
+#import <CGIJSONObject/NSData+CGIJSONSerializable.h>
+#import <CGIJSONObject/NSDate+CGIJSONSerializable.h>
+
+#import <CGIJSONObject/CGIRemoteObject.h>
+#import <CGIJSONObject/CGIRemoteConnection.h>
+
+#endif
